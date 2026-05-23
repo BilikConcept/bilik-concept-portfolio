bff96db8a24402e4f9624923dfffdb6c2417e74a
BilikConcept <office@bilikconcept.com>
2026-05-23 21:30:48 +0000

    update

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import Link from "next/link";
 import PublicBlackHeader from "@/components/PublicBlackHeader";
 import { getPublishedGalleries } from "@/lib/public-galleries";
